64d4a05c0adda352438f9c059648ea0097ca8fc2
Rogue.Ping <zhoujianping24@hotmail.com>
2026-02-15 10:36:39 +0800

    first try SPM

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SVGAPlayer_Optimized",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "SVGAPlayer_Optimized",
+            targets: ["SVGAPlayer_Optimized"]
+        )
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/Rogue24/SVGAPlayer-iOS.git",
+            branch: "SPM"
+        ),
+    ],
+    targets: [
+        .target(
+            name: "SVGAPlayer_Optimized",
+            dependencies: [
+                .product(
+                    name: "SVGAPlayer",
+                    package: "SVGAPlayer-iOS"
+                ),
+            ],
+            path: "SVGAPlayer_Optimized",
+            publicHeadersPath: ".",
+        )
+    ]
+)
